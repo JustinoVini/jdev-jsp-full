daa6420b8f4c033c0afdab2530a562d852f01f8d
Vinicius Justino <visilva5690@hotmail.com>
2023-08-27 22:05:06 -0300

    Segurança nas conexões do sistema

--- src/main/java/connection/SingleConnection.java
+++ src/main/java/connection/SingleConnection.java
@@ -1,44 +1,54 @@
 package connection;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 public class SingleConnection {
-	
-	private static String url = "jdbc:postgresql://localhost:5432/curso-jsp?autoReconnect=true";
-	private static String password = "";
-	private static String user = "postgres";
 	private static Connection connection = null;
-	
+
 	public static Connection connection() {
 		return connection;
 	}
-	
+
 	static {
 		conectar();
 	}
-	
+
 	public SingleConnection() {
 		conectar();
 	}
-	
+
 	private static void conectar() {
 		try {
-			
 			if (connection == null) {
-				Class.forName("org.postgresql.Driver"); /*Carrega o driver do banco*/
-				connection = DriverManager.getConnection(url, user, password);
-				connection.setAutoCommit(false); /*Para não fazer alterações no banco sem comando*/
+				Class.forName("org.postgresql.Driver"); /* Carrega o driver do banco */
+
+				// Especifique o caminho absoluto do arquivo database-config.txt
+				String filePath = "E:\\ws-jdev-curso\\jdev-jsp-full\\database-config.txt";
+
+				// Crie um objeto FileInputStream com o caminho absoluto
+				InputStream inputStream = new FileInputStream(filePath);
+
+				Properties properties = new Properties();
+				properties.load(inputStream);
+				String dbUrl = properties.getProperty("db.url");
+				String dbUser = properties.getProperty("db.username");
+				String dbPassword = properties.getProperty("db.password");
+
+				connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+				connection.setAutoCommit(false); /* Para não fazer alterações no banco sem comando */
 				System.out.println("Conectou com sucesso!");
 			}
-			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static Connection getConnection() {
 		return connection;
 	}
-	
 }
